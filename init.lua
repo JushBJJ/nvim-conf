a22e1fb3abf23a68d68de61105634468a0ed3e3d
Jush (yupiop12) <36951064+JushBJJ@users.noreply.github.com>
2024-09-07 18:26:43 +0000

    Add transparency

--- init.lua
+++ init.lua
@@ -20,3 +20,8 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
+
+vim.cmd[[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NonText guibg=NONE ctermbg=NONE
+]]
