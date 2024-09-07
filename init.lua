83292046bab0832b8c0316898e7133364fe82ce0
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2024-09-08 04:18:48 +1000

    added transparency compliance

--- init.lua
+++ init.lua
@@ -4,3 +4,19 @@ require("config.lazy")
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.g.autoformat = false
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  underline = false,
+  signs = false,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
